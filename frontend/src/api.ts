5f737f8612a2b9b5cbc0c00bc14b20140d9a8e39
Luke Hancock <lukehancock1@hotmail.com>
2024-07-24 18:39:03 +0100

    changed backend address

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import { Recipe } from "./types";
 
 export const searchRecipes = async(searchTerm:string, page:number) => {
-    const baseUrl = new URL("http://localhost:5000/api/recipes/search");
+    //const baseUrl = new URL("http://localhost:5000/api/recipes/search");
+    const baseUrl = new URL("https://food-recipe-app-backend-o14e.onrender.com/api/recipes/search");
     baseUrl.searchParams.append("searchTerm", searchTerm)
     baseUrl.searchParams.append("page",String(page))
 
@@ -14,7 +15,8 @@ export const searchRecipes = async(searchTerm:string, page:number) => {
 }
 
 export const getRecipeSummary= async (recipeId: string) => {
-    const url = new URL(`http://localhost:5000/api/recipes/${recipeId}/summary`)
+    //const url = new URL(`http://localhost:5000/api/recipes/${recipeId}/summary`)
+    const url = new URL(`https://food-recipe-app-backend-o14e.onrender.com/api/recipes/${recipeId}/summary`)
     const response = await fetch(url)
     if(!response.ok) { 
         throw new Error(`HTTP error! Status: ${response.status}`)
@@ -23,8 +25,8 @@ export const getRecipeSummary= async (recipeId: string) => {
 }
 
 export const getFavouriteRecipes = async() => {
-    const url = new URL('http://localhost:5000/api/recipes/favourite')
-
+    //const url = new URL('http://localhost:5000/api/recipes/favourite')
+    const url = new URL('https://food-recipe-app-backend-o14e.onrender.com/api/recipes/favourite')
     const response = await fetch(url);
     if (!response.ok){
         throw new Error(`HTTP error! Status: ${response.status}`)
@@ -34,7 +36,8 @@ export const getFavouriteRecipes = async() => {
 }
 
 export const addFavouriteRecipe = async(recipe: Recipe) => {
-    const url = new URL('http://localhost:5000/api/recipes/favourite')
+    //const url = new URL('http://localhost:5000/api/recipes/favourite')
+    const url = new URL('https://food-recipe-app-backend-o14e.onrender.com/api/recipes/favourite')
     const body = {
         recipeId: recipe.id
     }
@@ -54,7 +57,8 @@ export const addFavouriteRecipe = async(recipe: Recipe) => {
 }
 
 export const removeFavouriteRecipe = async(recipe: Recipe) => {
-    const url = new URL('http://localhost:5000/api/recipes/favourite')
+    //const url = new URL('http://localhost:5000/api/recipes/favourite')
+    const url = new URL('https://food-recipe-app-backend-o14e.onrender.com/api/recipes/favourite')
     const body = {
         recipeId: recipe.id
     }
